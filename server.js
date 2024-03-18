72b03dec57666774b343b3bb00515861123bc6f7
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-18 15:40:11 +0100

    Updated server.js to fix an issue

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-import("app.js");
+require("dotenv").config();
+const app = require("./app.js");
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
