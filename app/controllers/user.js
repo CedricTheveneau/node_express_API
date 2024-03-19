3b0c035c95df0c148ec021971921fb4220d7acb2
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-19 17:14:48 +0100

    Fixed an indentation issue in the user model, added the logic for the auth/signup route and implemented bcrypt password hashing
    Fixed an indentation issue in the user model, added the logic for the auth/signup route and implemented bcrypt password hashing

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,18 @@
-exports.signup = (req, res) => {
+const { User } = require("../models");
+const bcrypt = require("bcrypt");
+exports.signup = async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({
+      msg:
+        err.message ||
+        "Something wrong happened with your request to create a new user.",
+    });
+  }
+  console.log(req.body);
   res.send("You are signup");
 };
 
