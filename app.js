41efc5975889511d9b828c751cef2d8c52ff5ff8
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-19 16:23:47 +0100

    Added support for middleware with the express.json method in app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Initialization of the app with express.js
 const express = require("express");
-const app = express();
-
-// Initialize the connection with the DataBase
 const db = require("./app/models/index.js");
+const router = require("./app/routes/index.js");
+const app = express();
+app.use(express.json());
+
+// DB initialization
 db.sequelize
   .authenticate()
   .then(() => console.log("Database connected ..."))
   .catch((err) => console.log(err));
 
-// Initialize the router
-const router = require("./app/routes/index.js");
 //Add routes here
 app.use("/api", router);
 module.exports = app;
