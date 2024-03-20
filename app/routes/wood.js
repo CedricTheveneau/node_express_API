7e3e8bb0503a63071f58878df6f6270e7002309e
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-20 12:15:33 +0100

    Made the API private
    Created a middlewares folder, added an auth middleware that checks if a login token exists, if yes, gives acces to the data of the API, if no, returns an error : "acces denied !"

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express();
 const woodCtrl = require("../controllers/wood.js");
+const auth = require("../middlewares/auth.js");
 
-router.get("/", woodCtrl.list);
-router.get("/:hardness", woodCtrl.readByHardness);
+router.get("/", auth, woodCtrl.list);
+router.get("/:hardness", auth, woodCtrl.readByHardness);
 
 module.exports = router;
