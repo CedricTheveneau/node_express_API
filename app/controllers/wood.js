13a1d2ee66285b277336f2c060a7df02a4eb801c
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-20 10:41:13 +0100

    Added a route to fetch wood by hardness
    Added a route based on params where you enter the hardness of the wood you're looking for and returns all of the corresponding ones

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -11,3 +11,21 @@ exports.list = async (req, res) => {
     });
   }
 };
+
+exports.readByHardness = async (req, res) => {
+  try {
+    const hardness = req.params.hardness;
+    const woods = await Wood.findAll({
+      where: {
+        hardness: hardness,
+      },
+    });
+    res.status(201).json(woods);
+  } catch (err) {
+    res.status(500).json({
+      message:
+        err.message ||
+        "Something wrong happened with your request to retrieve the essences of wood based on the hardness you picked.",
+    });
+  }
+};
