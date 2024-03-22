03792930808aa79720e28c7915d48dc1b21efa1d
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-03-22 12:02:57 +0100

    Added support for Swagger API Documentation via yaml file
    Added support for Swagger API Documentation via yaml file
    
    Created documentation for Auth routes and some of the Woods routes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const db = require("./app/models/index.js");
 const router = require("./app/routes/index.js");
 const app = express();
 const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
 
 // DB initialization
 db.sequelize
@@ -11,9 +14,13 @@ db.sequelize
   .then(() => console.log("Database connected ..."))
   .catch((err) => console.log(err));
 
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Add routes here
 app.use("/api", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 module.exports = app;
